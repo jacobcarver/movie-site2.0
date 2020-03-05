a1f1049981100cfb83cca38b8b8e7cb76d8c3e39
Jacob Carver <jacxbcarver@gmail.com>
2020-03-05 14:06:17 -0700

    moved scss file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Container from './containers/Container';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import './scss/main.scss';
 
 import store from './redux/store';
 
